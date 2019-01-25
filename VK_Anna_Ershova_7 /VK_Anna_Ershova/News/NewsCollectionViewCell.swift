f219dfe66e0eba6790ac56cc9ede7bd3e716e9d3
Anna Ershova <anechka_ershova@macbook-air-anna.local>
2019-01-25 11:19:49 +0000

    7dz cells of news has been corrected

---
+++ VK_Anna_Ershova_7 /VK_Anna_Ershova/News/NewsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewsCollectionViewCell.swift
+//  VK_Anna_Ershova
+//
+//  Created by Anna Ershova on 1/22/19.
+//  Copyright © 2019 Anna Ershova. All rights reserved.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgNews: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
