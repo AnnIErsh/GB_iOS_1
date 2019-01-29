36a386631d8b66e5a0e6981a45323af87d1de251
Anna Ershova <anechka_ershova@macbook-air-anna.local>
2019-01-29 12:29:36 +0000

    8dz

--- VK_Anna_Ershova_7 /VK_Anna_Ershova/Animation/ImageAnimationView.swift
+++ VK_Anna_Ershova_7 /VK_Anna_Ershova/Animation/ImageAnimationView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ImageAnimationView: UIImageView {
     
+    var index = 0
+    var interactiveAnimator: UIViewPropertyAnimator!
     
     private func recognizer(_ sender: UIImageView){
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(animationImg))
@@ -24,6 +26,14 @@ class ImageAnimationView: UIImageView {
     
     
     @objc func animationImg() {
+        
+//        interactiveAnimator?.startAnimation()
+//        
+//        interactiveAnimator = UIViewPropertyAnimator(duration: 0.5,
+//                                                     dampingRatio: 0.5,
+//                                                     animations: {
+//                                                        self.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)  })
+
         let imageUp = CASpringAnimation(keyPath: "transform.scale")
         imageUp.fromValue = 1
         imageUp.toValue = 0.7
@@ -34,7 +44,7 @@ class ImageAnimationView: UIImageView {
         imageUp.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
         imageUp.fillMode = CAMediaTimingFillMode.backwards
         layer.add(imageUp, forKey: nil)
-        
+
         let imageDown = CASpringAnimation(keyPath: "transform.scale")
         imageDown.fromValue = 0.7
         imageDown.toValue = 1
@@ -47,7 +57,46 @@ class ImageAnimationView: UIImageView {
         layer.add(imageDown, forKey: nil)
     }
     
-    
+//    @objc func animationImg(_ recognizer: UIPanGestureRecognizer) {
+//        switch recognizer.state {
+//        case .began:
+//            interactiveAnimator?.startAnimation()
+//
+//            interactiveAnimator = UIViewPropertyAnimator(duration: 0.5,
+//                                                         dampingRatio: 0.5,
+//                                                         animations: {
+//                                                            self.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+//            })
+//            interactiveAnimator.pauseAnimation()
+//
+//        case .changed:
+//
+//            let translation = recognizer.translation(in: self)
+//
+//            interactiveAnimator.fractionComplete = translation.y / 100
+//                        if index == newsAll.count - 1 {
+//                            index = 0
+//                        }else{
+//                            index += 1
+//
+//                        }
+//
+//
+//
+//        case .ended:
+//            //interactiveAnimator.stopAnimation(true)
+//
+//            interactiveAnimator.addAnimations {
+//                self.transform = .identity
+//            }
+//            interactiveAnimator.startAnimation()
+//
+//            UICollectionViewCell.image = UIImage(named: newsAll[index])
+//
+//        default: return
+//        }
+//    }
+
     
     
     
--- VK_Anna_Ershova_7 /VK_Anna_Ershova/News/NewsCollectionViewController.swift
+++ VK_Anna_Ershova_7 /VK_Anna_Ershova/News/NewsCollectionViewController.swift
@@ -12,8 +12,21 @@ class NewsCollectionViewController: UICollectionViewController {
     
     var newsAll = ["Chloe", "Jade", "Sasha", "Yasmin", "Cameron", "Dipper", "Mabel", "Stanly"]
     
+    //var swImg = UIImageView()
+    
+    var index = 0
+    var interactiveAnimator: UIViewPropertyAnimator!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        self.view.addGestureRecognizer(recognizer)
+        
+        
         
     }
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -32,12 +45,15 @@ class NewsCollectionViewController: UICollectionViewController {
         
         let photo = newsAll[indexPath.row]
         cell.imgNews.image = UIImage(named: photo)
+        
+        
         return cell
     }
     
     override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         
-        
+  
+
         let opacityUp = CABasicAnimation(keyPath: "opacity")
         opacityUp.beginTime = CACurrentMediaTime()
         opacityUp.fromValue = 0
@@ -53,6 +69,9 @@ class NewsCollectionViewController: UICollectionViewController {
         scaleUp.duration = 0.8
         scaleUp.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
         cell.layer.add(scaleUp, forKey: nil)
+        
+        
+        
         
     }
     
@@ -73,8 +92,53 @@ class NewsCollectionViewController: UICollectionViewController {
         scaleDown.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
         cell.layer.add(scaleDown, forKey: nil)
         
+        
+        
+        
+        
     }
     
-    
-    
+
+    @objc func onPan(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            interactiveAnimator?.startAnimation()
+
+            interactiveAnimator = UIViewPropertyAnimator(duration: 0.5,
+                                                         dampingRatio: 0.5,
+                                                         animations: {
+                                                           self.view.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            })
+            interactiveAnimator.pauseAnimation()
+
+        case .changed:
+
+            let translation = recognizer.translation(in: self.view)
+
+            interactiveAnimator.fractionComplete = translation.y / 100
+//            if index == newsAll.count - 1 {
+//                index = 0
+//            }else{
+//                index += 1
+//
+//            }
+
+
+
+        case .ended:
+            interactiveAnimator.stopAnimation(true)
+
+            interactiveAnimator.addAnimations {
+                self.view.transform = .identity
+            }
+            interactiveAnimator.startAnimation()
+
+            //UICollectionViewCell.image = UIImage(named: newsAll[index])
+
+        default: return
+        }
+    }
+
+  
+
 }
